c5d31809dded4ebba9fea351219522b3f384a5d7
njmata <njmata@gmail.com>
2018-04-04 15:51:12 -0500

    Added requirements fields to app.js. Added new css rules.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        startDate: "",
+        numberDays: 0,
+        countryCode: ""
+    };
+
+    onStartDateChange = evt => {
+        this.setState({
+            startDate: evt.currentTarget.value
+        });
+    };
+
+    onNumberOfDayChange = evt => {
+        this.setState({
+            numberDays: evt.currentTarget.value
+        });
+    };
+
+    onCountryCodeChange = evt => {
+        this.setState({
+            countryCode: evt.currentTarget.value
+        });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <div className="requirement__field">
+                    <label>Start Date: </label>
+                    <input type="date" onChange={this.onStartDateChange} />
+                </div>
+                <div className="requirement__field">
+                    <label>Number of days: </label>
+                    <input type="number" onChange={this.onNumberOfDayChange} />
+                </div>
+                <div className="requirement__field">
+                    <label>Country Code: </label>
+                    <input type="text" onChange={this.onCountryCodeChange} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
